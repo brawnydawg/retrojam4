28293e3348cdf5b2d844438689f43a8853e3508d
brawnydawg <brawnydawg@icloud.com>
2022-03-17 03:44:40 -0400

    restart from scratch; basic main program

--- RocketRottie/rocketrottie.asm
+++ RocketRottie/rocketrottie.asm
@@ -9,35 +9,16 @@
         ; Our code starts at $0810 (2064 decimal)
         ; after the 15 bytes for the BASIC loader
 
+
 ;===============================================================================
 ; Initialize
 
-        ; Turn off interrupts to stop LIBSCREEN_WAIT failing every so 
-        ; often when the kernal interrupt syncs up with the scanline test
-        sei
-
-        ; Disable run/stop + restore keys
-        lda #$FC 
-        sta $0328
-
-        ; Set border and background colors
-        ; The last 3 parameters are not used yet
-        LIBSCREEN_SETCOLORS Blue, White, Black, Black, Black
-
-        ; Fill 1000 bytes (40x25) of screen memory 
-        LIBSCREEN_SET1000 SCREENRAM, 'a' ; 'a' maps to char 1
-
-        ; Fill 1000 bytes (40x25) of color memory
-        LIBSCREEN_SET1000 COLORRAM, Black
 
 ;===============================================================================
 ; Update
 
 gMLoop
-        LIBSCREEN_WAIT_V 255
-        ;inc EXTCOL ; start code timer change border color
-        ; Game update code goes here
-        ;dec EXTCOL ; end code timer reset border color
+
         jmp gMLoop
 
 
